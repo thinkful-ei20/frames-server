855fe5eed1e672897170cf414646b38c53041024
Cam Prier <CSPrier@gmail.com>
2018-07-17 13:32:25 -0700

    Created auth folder and strategies, modified and commented index.js and mounted routers, and created user routes

--- index.js
+++ index.js
@@ -1,26 +1,60 @@
 'use strict';
 
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const passport = require('passport');
+
+// Strategies
+const localStrategy = require('./auth/local');
+const jwtStrategy = require('./auth/jwt');
 
 const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
 
+// ROUTERS
+const userRouter = require('./users/routes/user');
+const authRouter = require('./users/routes/auth');
+
+// Express app
 const app = express();
 
+// Morgan
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
     skip: (req, res) => process.env.NODE_ENV === 'test'
   })
 );
 
+// CORS
 app.use(
   cors({
     origin: CLIENT_ORIGIN
   })
 );
 
+// Parse request body
+app.use(express.json());
+
+// Auth
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+// Endpoints
+app.use('/api', authRouter);
+app.use('/api/', userRouter);
+
+// Catch All 404
+app.use(function(req, res, next) {
+  const err = new Error('404 Not found');
+  err.status = 404;
+  console.error(err);
+  next(err);
+});
+
+// RUN SERVER
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
---
+++ users/routes/user.js
@@ -0,0 +1,138 @@
+'use strict';
+
+const express = require('express');
+
+const User = require('../../users/user');
+
+const router = express.Router();
+
+/* =================================================================================== */
+// CREATE NEW USER
+router.post('/', (req, res, next) => {
+  const requiredFields = ['username', 'email', 'companyname', 'password'];
+  const missingField = requiredFields.find(field => !(field in req.body));
+  
+  if (missingField) {
+    const err = new Error(`Missing ${missingField} in request body`);
+    err.status = 422;
+    console.error(err);
+    return next(err);
+  }
+
+  const stringFields = ['username', 'email', 'companyname','password'];
+  const nonStringField = stringFields.find(field => {
+    field in req.body && typeof req.body[field] !== 'string';
+  });
+
+  if (nonStringField) {
+    const err = new Error(`Field: '${nonStringField}' must be typeof String`);
+    err.status = 422;
+    console.error(err);
+    return next(err);
+  }
+
+  const trimmedFields = ['username', 'email', 'companyname', 'password'];
+  const nonTrimmedField = trimmedFields.find(field => {
+    req.body[field].trim() !== req.body[field];
+  });
+
+  if (nonTrimmedField) {
+    const err = new Error(`Field: '${nonTrimmedField}' cannot start or end with a whitespace!`);
+    err.status = 422;
+    console.error(err);
+    return next(err);
+  }
+
+  const sizedFields = {
+    username: { min: 1 },
+    email: { min: 1 },
+    companyname: { min: 1 },
+    password: { min: 8, max: 72 }
+  };
+
+  const tooSmall = Object.keys(sizedFields).find(field => {
+    'min' in sizedFields[field] 
+    && 
+    req.body[field].trim().length < sizedFields[field].min;
+  });
+  if (tooSmall) {
+    const min = sizedFields[tooSmall].min;
+    const err = new Error(`Field: '${tooSmall}' must be at least ${min} characters long`);
+    err.status = 422;
+    console.error(err);
+    return next(err);
+  }
+
+  const tooLarge = Object.keys(sizedFields).find(field => {
+    'max' in sizedFields[field] 
+    &&
+    req.body[field].trim().length > sizedFields[field].max;
+  });
+  if (tooLarge) {
+    const max = sizedFields[tooLarge].max;
+    const err = new Error(`Field: '${tooLarge}' must be at most ${max} characters long `);
+    err.status = 422;
+    console.error(err);
+    return next(err);
+  }
+
+  // Create the new user
+  let { username, email, companyname, password } = req.body;
+  
+  return User.hashPassword(password)
+    .then(digest => {
+      const newUser = {
+        username, 
+        email,
+        companyname,
+        password: digest
+      };
+      return User.create(newUser);
+    })
+    .then(result => {
+      return res.status(201)
+        .location(`/api/users/${result.id}`)
+        .json(result);
+    })
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('The username already exists');
+        err.status = 400;
+      }
+      console.error(err);
+      next(err);
+    });
+});
+
+/* =================================================================================== */
+// GET ALL USERS
+router.get('/', (req, res, next) => {
+  User.find()
+    .then(user => {
+      res.json(user);
+    })
+    .catch(err => {
+      console.error(err);
+      next(err);
+    });
+});
+
+/* =================================================================================== */
+// DELETE A USER BY ID
+router.delete('/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  User.findOneAndRemove({ _id: id })
+    .then(() => {
+      res.json({
+        message: 'Deleted user'
+      });
+      res.status(204).end();
+    })
+    .catch(err => {
+      console.error(err);
+      next(err);
+    });
+});
+
+module.exports = router;
