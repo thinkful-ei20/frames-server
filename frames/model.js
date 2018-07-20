e889355a59fa7fedec37a5f6529d728d21d64bc5
Marina Dargitz <marina.dargitz@gmail.com>
2018-07-20 13:35:23 -0600

    change employeeId on frame schema to not be required

--- frames/model.js
+++ frames/model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Define Frames Schema & Frames Model
 const frameSchema = new mongoose.Schema({
   adminId: { type: mongoose.Schema.Types.ObjectId, ref: 'Admin', required: true },
-  employeeId: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee', required: true },
+  employeeId: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee' },
   startFrame: { type: Date, require: true },
   endFrame: { type: Date, require: true }
 }, { timestamps: true });
