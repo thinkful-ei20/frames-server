315d79d0322e549ca7e5e014a6e2048f1d1ed19e
Marina Dargitz <marina.dargitz@gmail.com>
2018-07-20 10:11:23 -0600

    Adjust put router for the frame endpoint

--- frames/router.js
+++ frames/router.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const router = require('express').Router();
 const passport = require('passport');
@@ -8,15 +8,15 @@ const Employee = require('../users/models/employee');
 
 // Unprotected end point for testing purposes
 router.get('/test', (req, res, next) => {
-  Frame.find()
-    .then(result => {
-      if(result.length) {
-        res.json(result);
-      } else {
-        next();
-      }
-    })
-    .catch(next);
+	Frame.find()
+		.then(result => {
+			if(result.length) {
+				res.json(result);
+			} else {
+				next();
+			}
+		})
+		.catch(next);
 });
 
 // Protect endpoints using JWT Strategy
@@ -24,88 +24,88 @@ router.use('/', passport.authenticate('jwt', { session: false, failWithError: tr
 
 // Get all frames
 router.get('/', (req, res, next) => {
-  
-  const frameId = req.query.frameId;
-  const adminId = req.user.id;
-  const { startDate, endDate } = req.query;
 
-  const filter = { adminId };
+	const frameId = req.query.frameId;
+	const adminId = req.user.id;
+	const { startDate, endDate } = req.query;
 
-  // Only filter on startDate and endDate if they are provided
-  if(startDate) {
-    filter.startFrame = { $gte: startDate };
+	const filter = { adminId };
 
-  } else if(endDate) {
-    filter.endFrame = { $lte: endDate };
-  }
+	// Only filter on startDate and endDate if they are provided
+	if(startDate) {
+		filter.startFrame = { $gte: startDate };
 
-  Frame.find(filter)
-    .populate('employeeId')
-    .then(results => {
-      console.log('GET RESULTS', results);
-      if(results.length) {
-        res.json(results);
-      } else {
-        next();
-      }
-    })
-    .catch(err => {
-      next(err);
-    });
+	} else if(endDate) {
+		filter.endFrame = { $lte: endDate };
+	}
+
+	Frame.find(filter)
+		.populate('employeeId')
+		.then(results => {
+			console.log('GET RESULTS', results);
+			if(results.length) {
+				res.json(results);
+			} else {
+				next();
+			}
+		})
+		.catch(err => {
+			next(err);
+		});
 });
 
 // Get all frames for one employee
 router.get('/:id', (req, res, next) => {
 
-  const employeeId = req.params.id;
-  const adminId = req.user.id;
+	const employeeId = req.params.id;
+	const adminId = req.user.id;
 
-  /***** Never trust users - validate input *****/
-  if (!mongoose.Types.ObjectId.isValid(employeeId)) {
-    const err = new Error(`The employee id ${employeeId} is not valid`);
-    err.status = 400;
-    return next(err);
-  }
+	/***** Never trust users - validate input *****/
+	if (!mongoose.Types.ObjectId.isValid(employeeId)) {
+		const err = new Error(`The employee id ${employeeId} is not valid`);
+		err.status = 400;
+		return next(err);
+	}
 
-  Frame.find({
-    adminId,
-    employeeId,
-  })
-    .then(result => {
-      if (result.length) {
-        res.json(result);
-      } else {
-        next();
-      }
-    })
-    .catch(next);
+	Frame.find({
+		adminId,
+		employeeId,
+	})
+		.then(result => {
+			if (result.length) {
+				res.json(result);
+			} else {
+				next();
+			}
+		})
+		.catch(next);
 });
 
 // Get a single frame by frameId
 router.get('/frame/:id', (req, res, next) => {
 
-  const adminId = req.user.id;
-  const frameId = req.params.id;
+	const adminId = req.user.id;
+	const frameId = req.params.id;
 
-  /***** Never trust users - validate input *****/
-  if (!mongoose.Types.ObjectId.isValid(frameId)) {
-    const err = new Error(`The frame id ${frameId} is not valid`);
-    err.status = 400;
-    return next(err);
-  }
+	/***** Never trust users - validate input *****/
+	if (!mongoose.Types.ObjectId.isValid(frameId)) {
+		const err = new Error(`The frame id ${frameId} is not valid`);
+		err.status = 400;
+		return next(err);
+	}
 
-  Frame.findOne({
-    _id: frameId,
-    adminId
-  })
-    .then(result => {
-      if(result) {
-        res.json(result);
-      } else {
-        next();
-      }
-    })
-    .catch(next);
+	Frame.findOne({
+		_id: frameId,
+		adminId
+	})
+		.then(result => {
+			if(result) {
+				res.json(result);
+			} else {
+				next();
+			}
+		})
+		.catch(next);
 
 
 });
@@ -113,117 +113,93 @@ router.get('/frame/:id', (req, res, next) => {
 // Post/create a frame
 router.post('/frame', (req, res, next) => {
 
-  const requiredFields = ['employeeId', 'startFrame', 'endFrame'];
-  const missingField = requiredFields.find(field => !(field in req.body));
+	const requiredFields = ['employeeId', 'startFrame', 'endFrame'];
+	const missingField = requiredFields.find(field => !(field in req.body));
 
-  if (missingField) {
-    const err = new Error(`Missing ${missingField} in request body`);
-    err.status = 422;
-    console.error(err);
-    return next(err);
-  }
+	if (missingField) {
+		const err = new Error(`Missing ${missingField} in request body`);
+		err.status = 422;
+		console.error(err);
+		return next(err);
+	}
 
-  const stringFields = ['employeeId', 'startFrame', 'endFrame'];
-  const nonStringField = stringFields.find(
-    field => (field in req.body && typeof req.body[field]) !== 'string'
-  );
+	const stringFields = ['employeeId', 'startFrame', 'endFrame'];
+	const nonStringField = stringFields.find(
+		field => (field in req.body && typeof req.body[field]) !== 'string'
+	);
 
-  if (nonStringField) {
-    const err = new Error(`Field: '${nonStringField}' must be typeof String`);
-    err.status = 422;
-    console.error(err);
-    return next(err);
-  }
+	if (nonStringField) {
+		const err = new Error(`Field: '${nonStringField}' must be typeof String`);
+		err.status = 422;
+		console.error(err);
+		return next(err);
+	}
 
 
-  const adminId = req.user.id;
-  const { employeeId, startFrame, endFrame } = req.body;
+	const adminId = req.user.id;
+	const { employeeId, startFrame, endFrame } = req.body;
 
-  const frame = {
-    adminId,
-    employeeId,
-    startFrame,
-    endFrame
-  };
+	const frame = {
+		adminId,
+		employeeId,
+		startFrame,
+		endFrame
+	};
 
-  console.log('FRAME', frame);
-  Frame.create(frame)
-    .then(result => {
-      res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
-    })
-    .catch(next)
+	console.log('FRAME', frame);
+	Frame.create(frame)
+		.then(result => {
+			res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+		})
+		.catch(next);
 });
 
 // Update a single frame
 router.put('/frame/:id', (req, res, next) => {
-  const adminId = req.user.id;
-  const frameId = req.params.id;
-  const { startDate, endDate } = req.body;
+	const adminId = req.user.id;
+	const frameId = req.params.id;
+	const updateableFields = ['startFrame', 'endFrame', 'employeeId'];
+	const updatedShift = {};
+	updateableFields.map(field => {
+		if (field in req.body){
+			updatedShift[field] = req.body[field];
+		}
+	});
 
-  /***** Never trust users - validate input *****/
-  if (!mongoose.Types.ObjectId.isValid(frameId)) {
-    const err = new Error(`The frame id ${frameId} is not valid`);
-    err.status = 400;
-    return next(err);
-  }
+	/***** Never trust users - validate input *****/
+	if (!mongoose.Types.ObjectId.isValid(frameId)) {
+		const err = new Error(`The frame id ${frameId} is not valid`);
+		err.status = 400;
+		return next(err);
+	}
 
-  const requiredFields = ['startDate', 'endDate'];
-  const missingField = requiredFields.find(field => !(field in req.body));
-
-  if (missingField) {
-    const err = new Error(`Missing ${missingField} in request body`);
-    err.status = 422;
-    console.error(err);
-    return next(err);
-  }
-
-  const fields = {
-    startFrame: startDate,
-    endFrame: endDate };
-
-  Frame.findOne({ _id: frameId, adminId })
-    // .populate('adminId')
-    .populate('employeeId', 'lastname')
-    .then(result => {
-      console.log('RESULT', result.employeeId.lastname);
+	Frame.findOneAndUpdate({ _id: frameId, adminId }, updatedShift, {new: true})
+		.then(result => {
+			res.json(result);
+		});
 
 
-
-    })
-
-
-  // Frame.findOne({ _id: frameId, adminId })
-  //   .then(result => {
-  //     if(!result) {
-  //       return next();
-  //     }
-  //     return Frame.findByIdAndUpdate(frameId, fields, { new: true })
-  //   })
-  //   .then(frame => {
-  //     return res.json(frame);
-  //   })
-  //   .catch(next);
 });
 
 // Delete a single frame
 router.delete('/frame/:id', (req, res, next) => {
-  const adminId = req.user.id;
-  const frameId = req.params.id;
+	const adminId = req.user.id;
+	const frameId = req.params.id;
 
-  /***** Never trust users - validate input *****/
-  if (!mongoose.Types.ObjectId.isValid(frameId)) {
-    const err = new Error(`The frame id ${frameId} is not valid`);
-    err.status = 400;
-    return next(err);
-  }
+	/***** Never trust users - validate input *****/
+	if (!mongoose.Types.ObjectId.isValid(frameId)) {
+		const err = new Error(`The frame id ${frameId} is not valid`);
+		err.status = 400;
+		return next(err);
+	}
 
-  Frame.findOneAndRemove({ _id: frameId, adminId })
-    .then(() => {
-      res.status(204).end();
-    })
-    .catch(err => {
-      next(err);
-    });
+	Frame.findOneAndRemove({ _id: frameId, adminId })
+		.then(() => {
+			res.status(204).end();
+		})
+		.catch(err => {
+			next(err);
+		});
 });
 
 module.exports = router;
