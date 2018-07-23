9501d6cd7a6e8f7c9249d0fe56fb65d16bb75980
Marina Dargitz <marina.dargitz@gmail.com>
2018-07-23 13:25:45 -0600

    Add get single employee endpoint

--- users/routes/employee.js
+++ users/routes/employee.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const router = require('express').Router();
 const passport = require('passport');
@@ -9,18 +9,33 @@ router.use('/', passport.authenticate('jwt', { session: false, failWithError: tr
 
 // Get all employees
 router.get('/', (req, res, next) => {
-  const adminId = req.user.id;
+	const adminId = req.user.id;
 
-  Employee.find({adminId})
-    .sort('lastname')
-    .then(result => {
-      if(result.length) {
-        res.json(result);
-      } else {
-        next();
-      }
-    })
-    .catch(next);
+	Employee.find({adminId})
+		.sort('lastname')
+		.then(result => {
+			if(result.length) {
+				res.json(result);
+			} else {
+				next();
+			}
+		})
+		.catch(next);
+});
+
+// Get a single employee by ID
+router.get('/:employeeId', (req,res,next) => {
+	const adminId = req.user.id;
+	const {employeeId} = req.params;
+
+	Employee.findById(employeeId)
+		.then(result => {
+			if(result){
+				return res.json(result);
+			}
+			next();
+		})
+		.catch(error => next(error));
 });
 
 module.exports = router;
